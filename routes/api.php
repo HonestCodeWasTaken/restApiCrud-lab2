6d03aab993465cde1b38116f94ef0f0916c773d3
Tomas Radionovas <Tomas.Radionovas@ktu.edu>
2021-11-01 11:20:58 +0200

    refactored into controllers

--- routes/api.php
+++ routes/api.php
@@ -21,70 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/messages', function () {
-    return messages::all();
-});
-Route::get('/users', function () {
-    return users::all();
-});
-Route::post('/messages', function () {
-    request()->validate([
-        'message' => 'required',
-        'whoSent_ID' => 'required',
-        'receiver_ID' => 'required',
-    ]);
-    return messages::create(
-        [
-            'message' => request('message'),
-            'whoSent_ID' => request('whoSent_ID'),
-            'receiver_ID' => request('receiver_ID'),
-        ]
-    );
-});
-Route::post('/users', function () {
-    request()->validate([
-        'username' => 'required',
-        'email' => 'required'
-    ]);
-    return users::create(
-        [
-            'username' => request('username'),
-            'email' => request('email')
-        ]
-    );
-});
-Route::put('/messages/{message}', function (messages $message) {
-    request()->validate([
-        'message' => 'required'
-    ]);
-    $message->update([
-        'message' => request('message')
-    ]);
-});
-Route::put('/users/{user}', function (users $user) {
-    request()->validate([
-        'username' => 'required',
-        'email' => 'required'
-    ]);
-    $user->update([
-        'username' => request('username'),
-        'email' => request('email')
-    ]);
-});
-Route::delete('/users/{user}', function (users $user) {
-    $success = $user->delete();
 
-    return [
-        'success' => $success,
-    ];
-});
-Route::delete('/messages/{message}', function (messages $message) {
-    $success = $message->delete();
-
-    return [
-        'success' => $success,
-    ];
-});
 
 Route::get('/messages', [MessagesApiController::class, 'index']);
 Route::post('/messages', [MessagesApiController::class, 'store']);
