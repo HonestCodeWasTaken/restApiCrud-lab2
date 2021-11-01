ba93b0d1b0f070ed7e873cb182ec6e47202e8fc0
Tomas Radionovas <Tomas.Radionovas@ktu.edu>
2021-11-01 17:29:14 +0200

    login page

--- chat-app-client/pages/_app.tsx
+++ chat-app-client/pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import { ChakraProvider } from '@chakra-ui/react'
+import { ToastProvider, useToasts } from 'react-toast-notifications'
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (<ToastProvider>
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  </ToastProvider>)
+
 }
 
 export default MyApp
---
+++ chat-app-client/pages/api/users.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export default class UsersSVC {
+    public static uploadUser = async (username:string, email:string, url: string) => {
+        axios.post(`${url}/users`,{
+            "username": username,
+            "email": email
+         }).then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
+    public static fetchUsers = async (url:string) => {
+        return fetch(url).then(r => r.json())
+    }
+}
