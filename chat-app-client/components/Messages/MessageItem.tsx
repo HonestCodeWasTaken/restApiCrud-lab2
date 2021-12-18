5ea932e8f7b11aeb98938e02a17110b3286d6acd
Tomas Radionovas <Tomas.Radionovas@ktu.edu>
2021-12-18 16:19:33 +0200

    gynimas

--- chat-app-client/components/Messages/MessageItem.tsx
+++ chat-app-client/components/Messages/MessageItem.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
 	Flex,
 	Text,
@@ -8,6 +8,7 @@ import {
 	Avatar,
 	Heading
 } from '@chakra-ui/react'
+import MessagesSVC from '../../pages/api/MessagesSVC'
 
 interface IMessageProps {
 	name:string | undefined;
@@ -17,6 +18,23 @@ interface IMessageProps {
 	checkbox:string;
 }
 export const MessageItem: React.FC<IMessageProps> = (props: IMessageProps) => {
+  const [ip, setIp] = useState<any>()
+const getIp = async () => {
+  
+  let ip;
+  ip = await fetch('https://ipapi.co/json/')
+  .then(function(response) {
+    return response.json();
+  })
+  debugger;
+  // ip = await fetch("https://www.cloudflare.com/cdn-cgi/trace")
+	// ip = await MessagesSVC.fetchUrl("https://ipinfo.io");
+  setIp(ip)
+  
+}
+	useEffect(() => {
+		getIp();
+	}, []);
 	return (
 		<Flex mt={1} align="center">
 			<Avatar size="sm" src="avatar-1.jpg" />
@@ -24,6 +42,7 @@ export const MessageItem: React.FC<IMessageProps> = (props: IMessageProps) => {
 				<span style={{ display: "flex" }}>
 					<Heading color="white" as="h3" size="sm">{props.name}</Heading>
 					<Heading fontSize="11px" color="#9b9797" style={{ marginTop: "6px" }} ml={2} as="h3" size="xs">{new Date(props.time).toUTCString()}</Heading>
+					{ip && <Heading fontSize="11px" color="#9b9797" style={{ marginTop: "6px" }} ml={2} as="h3" size="xs">{ip.ip}</Heading>}
 				</span>
 
 				<Text fontSize="14px" color="#DCDAD9">{props.message}</Text>
